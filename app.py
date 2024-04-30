b0d3e262e812a95ad44532a7e52c10d34959c53c
Nischal <vadlan@pedarecc.sa.edu.au>
2024-04-30 13:03:41 +0930

    Displaying data on web

--- app.py
+++ app.py
@@ -1,9 +1,54 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+import requests
+
+
 app = Flask(__name__)
 
 @app.route('/')
+
+
 def home():
-    return render_template('home.html')
+    api_key = "4ccc7cabc4bac53437084c5a6f2e1781"
+    city = 'Adelaide'
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&APPID={api_key}&units=metric"
+    print(url)
+    response = requests.get(url)
+    data = response.json()
+
+
+
+        # "description": data["weather"][0]["description"],
+        # "temperature": data["main"]["temp"],n
+        # "humidity": data["main"]["humidity"],
+        # "wind_speed": data["wind"]["speed"]}
+
+    forecast_list = data['list']
+    index = 0
+    forecast_data = []
+    while index < len(forecast_list):
+
+        dt_txt = forecast_list[index]["dt_txt"]
+        temp = forecast_list[index]["main"]["temp"]
+        icon = forecast_list[index]["weather"][0]["icon"]
+        description = forecast_list[index]["weather"][0]["description"]
+        index +=8
+
+        today = datetime(2017, 10, 20)
+        today.get_weekday()
+
+        thisdict = {
+                "dt_txt": dt_txt,
+                "temp": temp,
+                "icon": "http://openweathermap.org/img/w/" + icon + ".png",
+                "description": description
+            }
+        forecast_data.append(thisdict)
+        print(forecast_data)
+
+
+        return render_template('home.html',forecast_data=forecast_data)
+
 
 
 
